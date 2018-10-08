67f8b9d96c0951006452086976669ddc041b8296
Thomas Grimes <tgrimes.westshoresolutions@gmail.com>
2018-10-08 12:39:24 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state={
+    squares:[
+      ['','',''],
+      ['','',''],
+      ['','','']
+    ],
+    player:'X'
+  }
+
+  handleUpdate=(row,col)=>{
+    this.state.squares[row][col] = this.state.player==='X'?'X':'O'
+    this.setState({squares:this.state.squares,player:this.state.player==='X'?'O':'X'})
+    this.checkForWinner(row,col)
+  }
+
+  checkForWinner=(row,col)=>{
+    const {squares,player} = this.state
+    switch(true){
+      case squares[0][0]===player&&squares[0][1]===player&&squares[0][2]===player: {
+        this.winner()
+        break
+      }
+      case squares[0][col]===player&&squares[1][col]===player&&squares[2][col]===player:{
+        this.winner()
+        break
+      }
+      case squares[0][0]===player&&squares[1][1]===player&&squares[2][2]===player:{
+        this.winner()
+        break
+      }
+      case squares[0][2]===player&&squares[1][1]===player&&squares[2][0]===player:{
+        this.winner()
+        break
+      }
+      default:return
+    }
+  }
+
+  winner=()=>{
+    alert(`${this.state.player} is the winner!`)
+    this.setState({squares:[[null,null,null],[null,null,null],[null,null,null]]})
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.squares.map((elem1,i)=>(
+          <div key={i}>
+            {elem1.map((elem2,j)=>(
+              <div key={j} onClick={()=>this.handleUpdate(i,j)} className="box inlineBox">
+                <div className="item">
+                  {elem2}
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     );
   }
 }
+
 
 export default App;
